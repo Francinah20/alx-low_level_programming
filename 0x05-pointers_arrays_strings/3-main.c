e0522cdb338592f638c0babca4b8dbe430bbd639
Francinah20 <118854529+Francinah20@users.noreply.github.com>
2022-12-19 13:56:57 +0200

    Update 3-main.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,20 @@
-#include "main.h"
+include "main.h"
 
 /**
- * main - check the code
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
  *
- * Return: Always 0.
+ * Return: 0
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
